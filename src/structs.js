871d46452b58658d92799173a4c906f5f153ea89
阿卡琳 <hufan.akarin@Gmail.com>
2019-08-06 21:37:14 +0800

    feat: add JavaScript config

---
+++ src/structs.js
@@ -0,0 +1,27 @@
+// Definition for singly-linked list.
+
+export class ListNode {
+    /**
+     * @param {number} val
+     * @param {ListNode | null} next
+     */
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// Definition for a binary tree node.
+// Definition for a binary tree node.
+export class TreeNode {
+    /**
+     * @param {number} val
+     * @param {TreeNode | null} left
+     * @param {TreeNode | null} right
+     */
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
